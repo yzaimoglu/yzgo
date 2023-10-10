729ad24a9a9f76dd72def4749bf40d23c5f734fc
Yagizalp <yagizalp@zaimoglu.de>
2023-10-11 01:22:41 +0200

    fix: fixed a small bug in sqlite driver

--- data/database/sqlite.go
+++ data/database/sqlite.go
@@ -1,23 +1,70 @@
 package data
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gookit/slog"
+	"github.com/yzaimoglu/yzgo/config"
+	data "github.com/yzaimoglu/yzgo/data/const"
+	"github.com/yzaimoglu/yzgo/utils"
+
+	_ "github.com/glebarez/go-sqlite"
+)
 
 type SQLiteConfig struct {
-	Host      string
-	Port      int
-	User      string
-	Password  string
-	Namespace string
-	Database  string
+	Database string
 }
 
 type SQLite struct {
-	Config   *SurrealDBConfig
+	Config   *SQLiteConfig
 	Database *sql.DB
 }
 
 func NewSQLite() *SQLite {
-	return &SQLite{}
+ConnectionStart:
+	if config.GetBoolean(data.EnvDBEnabled) {
+		dbConfig := SQLiteConfig{
+			Database: config.GetString(data.EnvDBDatabase),
+		}
+
+		if _, err := os.Stat("db"); os.IsNotExist(err) {
+			if err := os.MkdirAll("db", 0755); err != nil {
+				slog.Errorf("Error creating db directory: %v", err)
+				slog.Errorf("Retrying in 10 seconds...")
+				time.Sleep(10 * time.Second)
+				config.Load()
+				goto ConnectionStart
+			}
+		}
+
+		path := fmt.Sprintf("db/%s.db?_pragma=journal_mode(WAL)&_pragma=busy_timeout(5000)", dbConfig.Database)
+		db, err := sql.Open("sqlite", path)
+		if err != nil {
+			slog.Errorf("Error connecting to SQLite: %v", err)
+			slog.Errorf("Retrying in 10 seconds...")
+			time.Sleep(10 * time.Second)
+			config.Load()
+			goto ConnectionStart
+		}
+
+		utils.NonChildExec(func() {
+			var version string
+			versionQuery := db.QueryRow("select sqlite_version()")
+			if err := versionQuery.Scan(&version); err != nil {
+				slog.Errorf("Error connecting to SQLite: %v", err)
+				panic(err)
+			}
+			slog.Infof("Connected to sqlite v%s at path %s", version, path)
+		})
+		return &SQLite{
+			Config:   &dbConfig,
+			Database: db,
+		}
+	}
+	return nil
 }
 
 func (db *SQLite) Init() error {
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.21.0
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/glebarez/go-sqlite v1.21.2 // indirect
 	github.com/gofiber/fiber/v2 v2.49.2 // indirect
 	github.com/google/uuid v1.3.1 // indirect
 	github.com/gookit/color v1.5.4 // indirect
@@ -17,6 +19,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/rqlite/gorqlite v0.0.0-20230708021416-2acd02b70b79 // indirect
 	github.com/stripe/stripe-go/v74 v74.30.0 // indirect
@@ -28,6 +31,10 @@ require (
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	modernc.org/libc v1.24.1 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.7.2 // indirect
+	modernc.org/sqlite v1.26.0 // indirect
 )
